5f40993bbf5ec5a82c4e236b6bf0679ef7b6da2d
icdong <i@icdong.com>
2021-07-08 00:01:54 +0800

    测试

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,12 @@
  * @Author: Daito Chai
  * @Date: 2021-07-07 23:21:21
  * @LastEditors: Daito Chai
- * @LastEditTime: 2021-07-07 23:29:32
+ * @LastEditTime: 2021-07-08 00:01:45
  */
 const router = require('koa-router')()
 
 // router.prefix('/users')
+
 
 router.get('/getData', function (ctx, next) {
     ctx.body = 'this is a users/bar response'
