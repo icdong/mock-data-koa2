6485baf11eae07f95f006082fdba798bc1af63b6
chaichengdong <chaichengdong@mxbc.com>
2021-07-09 08:45:23 +0800

    上传成功

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,25 @@
  * @Author: Daito Chai
  * @Date: 2021-07-08 08:43:07
  * @LastEditors: Daito Chai
- * @LastEditTime: 2021-07-08 09:21:03
+ * @LastEditTime: 2021-07-09 08:42:35
  */
 const router = require('koa-router')()
+
+const upload = require('./upload')
 
 router.get('/', async (ctx, next) => {
     await ctx.render('index', {
         title: 'Hello Koa 2!'
     })
+})
+
+// 上传资料图片
+router.post('/upload', upload.single('file'), async ctx => {
+    ctx.body = {
+        file: ctx.req.file,
+        code: 1000,
+        desc: 'success'
+    }
 })
 
 module.exports = router
--- routes/upload.js
+++ routes/upload.js
@@ -1,12 +1,11 @@
 /*
- * @Description: 上传图片接口demo
+ * @Description: 上传图片后保存到本地
  * @Author: Daito Chai
  * @Date: 2021-07-08 23:01:10
  * @LastEditors: Daito Chai
- * @LastEditTime: 2021-07-08 23:05:24
+ * @LastEditTime: 2021-07-09 08:42:10
  */
 const multer = require('koa-multer')
-const router = require('koa-router')()
 
 let storage = multer.diskStorage({
     //文件保存路径 这个路由是以项目文件夹 也就是和入口文件（如app.js同一个层级的）
@@ -19,14 +18,6 @@ let storage = multer.diskStorage({
     }
 })
 
-router.post('/upload', multer({ storage }).single('file'), async ctx => {
-    console.log(ctx.req.file)
-
-    ctx.body = {
-        file: ctx.req.file,
-        code: 1000,
-        desc: 'success'
-    }
+module.exports = multer({
+    storage
 })
-
-module.exports = router
